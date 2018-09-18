658a37920a43c9b32780f891550454c6a5056825
dubd2206 <daniel-junior.dube@usherbrooke.ca>
2018-09-18 17:34:08 -0400

    Add build.rs script to pull git submodules on build

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+	let cargo_manifest_dir = &env::var("CARGO_MANIFEST_DIR").unwrap();
+	let current_directory = Path::new(&cargo_manifest_dir);
+
+	println!("Cargo:current_directory: {:?}", current_directory);
+
+	Command::new("git")
+		.arg("submodule")
+		.arg("update")
+		.arg("--init")
+		.current_dir(&current_directory)
+		.status()
+		.unwrap();
+}
