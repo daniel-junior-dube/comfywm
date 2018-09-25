dd67301254c15f485386303f4d6bc944bf2aae2e
dubd2206 <daniel-junior.dube@usherbrooke.ca>
2018-09-25 16:28:23 -0400

    Add comment on dependency script to describe dependency compilation from source

--- scripts/ubuntu/install_ubuntu_dependencies.sh
+++ scripts/ubuntu/install_ubuntu_dependencies.sh
@@ -4,6 +4,7 @@
 FILENAME="./dependencies.txt"
 apt-get install $(grep -vE "^\s*#" $FILENAME  | tr "\n" " ")
 
+# Compile/install wayland 1.15.0-2 from source
 wget "https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/wayland/1.15.0-2/wayland_1.15.0.orig.tar.gz" -O ./wayland-latest.tar.gz
 tar -xf ./wayland-latest.tar.gz
 pushd wayland-latest
@@ -13,6 +14,7 @@ make install
 popd
 rm -rf ./wayland-latest.tar.gz ./wayland-latest
 
+# Compile/install wayland-protocols 1.15.1 from source
 wget "https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/wayland-protocols/1.15-1/wayland-protocols_1.15.orig.tar.xz" -O ./wayland-protocols-latest.tar.gz
 tar -xf ./wayland-protocols-latest.tar.gz
 pushd wayland-protocols-latest
