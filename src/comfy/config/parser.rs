6fab3d48f51aac9e0ddc0cf364ccafef205dab42
Chabam <fchabot1337@gmail.com>
2018-10-22 17:14:28 -0400

    Code fixes from the merge request #12

--- src/comfy/config/parser.rs
+++ src/comfy/config/parser.rs
@@ -5,13 +5,13 @@ pub fn convert_to_xkb_string(modkey_str: &str, keyset: &str) -> Result<Vec<Strin
 	// Replace all the $mod in the file with the value of modkey
 	let splitted_keyset: Vec<&str> = keyset
 		.split("+")
-		.map(|key| {
+		.map(|key|
 			if key == "$mod" {
-				return modkey_str;
+				modkey_str
 			} else {
-				return key;
+				key
 			}
-		}).collect();
+		).collect();
 
 	let mut cannonicalized_keys: Vec<Vec<String>> = Vec::new();
 	for key in splitted_keyset.iter() {
@@ -31,8 +31,6 @@ pub fn convert_to_xkb_string(modkey_str: &str, keyset: &str) -> Result<Vec<Strin
 ///               "Control_L+Shift_R+Up",
 ///               "Control_L+Shift_L+Up"]```
 fn create_combinations_as_string(keys: Vec<Vec<String>>) -> Vec<String> {
-	let mut combinations: Vec<String> = Vec::new();
-
 	// Split the keys in two groups
 	// Ex: [["Control_L", "Control_R"], ["Shift_L", "Shift_R"], ["Up"]] =>
 	// key_head_group = ["Control_L", "Control_R"]
@@ -45,8 +43,10 @@ fn create_combinations_as_string(keys: Vec<Vec<String>>) -> Vec<String> {
 
 	// Base case: return the prefix if the remainder is empty.
 	if key_tail_group.len() == 0 {
-		return prefix_keys;
+		prefix_keys
 	} else {
+		let mut combinations: Vec<String> = Vec::new();
+
 		// Recursive step: call the function it self with the remainder until it is empty.
 		let remainder_key_combinations: Vec<String> = create_combinations_as_string(key_tail_group);
 
@@ -56,9 +56,9 @@ fn create_combinations_as_string(keys: Vec<Vec<String>>) -> Vec<String> {
 				combinations.push(vec![inital_key.clone(), key.clone()].join("+"));
 			}
 		}
-	}
 
-	combinations
+		combinations
+	}
 }
 
 /// Convert a Comfy's modifier class into a valid XkbKeySet if it needs to.
